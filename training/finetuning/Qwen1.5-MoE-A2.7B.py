6589e125a4438bcb6ae6a063f40d92eba4071213
Đinh Trác Đức Anh <dinhtracducanh@MacBook-Air-cua-inh.local>
2026-08-19 13:23:46 +0700

    fix: optimize optimizer

--- training/finetuning/Qwen1.5-MoE-A2.7B.py
+++ training/finetuning/Qwen1.5-MoE-A2.7B.py
@@ -784,7 +784,8 @@ def main():
 
     # ------------------------------------------------------------------------------- optimizer
     trainable_params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.AdamW(trainable_params, lr=args.learning_rate, weight_decay=args.weight_decay)
+    optimizer = torch.optim.AdamW(trainable_params, lr=args.learning_rate,
+                                weight_decay=args.weight_decay, foreach=True)  # hoặc fused=True nếu CUDA hỗ trợ
 
     steps_per_epoch = len(batch_sampler)
     total_steps = steps_per_epoch * args.num_train_epochs
